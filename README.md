7613572cb3f2f4566446c10574b48cb3fbbd4c93
WanWuIO <noreply@wanwu.io>
2022-02-11 19:04:29 +0800

    create project

---
+++ README.md
@@ -0,0 +1,9 @@
+# CashNFT-token-list
+
+This GitHub repo contains the default token list used in the WanWu CashNFT.
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
+
+Copyright (c) WanWu.io
---
+++ src/buildList.js
@@ -0,0 +1,19 @@
+const { version } = require('../package.json')
+const smartbch = require('./tokens/smartbch.json')
+
+module.exports = function buildList () {
+  const parsed = version.split('.')
+  return {
+    name: 'WanWu CashNFT List',
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
+    },
+    tags: {},
+    logoURI: '',
+    keywords: ["wanwu-cashnft", "default"],
+    tokens: [...smartbch]
+  }
+}
